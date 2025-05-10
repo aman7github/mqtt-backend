213ea0b0c33e0e85dc52c5069a84401559dd4cb8
aman7github <amanmaurya9044@gmail.com>
2025-05-10 20:11:13 +0530

    change mqtt url

--- mqtt/mqttHandler.js
+++ mqtt/mqttHandler.js
@@ -1,89 +1,3 @@
-// const mqtt = require('mqtt');
-// const { redisClient } = require('../redis/redisClient');
-// const { TaskModel } = require('../mongo/task.model');
-// require('dotenv').config()
-
-//  const Task_Key = 'FULLSTACK_TASK_AMAN';
-
-// function mqttHandler(){
-//   const client = mqtt.connect('mqtt://broker.hivemq.com')
-
-//     client.on('connect',()=>{
-
-//         console.log('Connected to MQTT Broker');
-
-//         client.subscribe('/add',(err)=>{
-//              if(err){
-//                 console.log(err.message);
-//              }else{
-//                 console.log('subscribe to /add');
-                
-//              }
-//         })
-//     })
-
-
-    
-//      client.on('message',async(topic,message)=>{
-   
-//      const task = message.toString()
-    
-//        try{
-//          if(JSON.parse(task)){
-//          console.log('task is json string invalid');
-//          return
-//          }
-//       }catch(err){
-//         console.log(err.message);  
-//      }
-   
-
-//     try{
-//      // getting tasklist from redis and pushing new task 
-//         let taskList = await redisClient.get(Task_Key)
-//         taskList = taskList? JSON.parse(taskList):[]
-//         taskList.push(task)
-//         console.log('checking redis', taskList);
-       
-//      // for mongoDB convert task arry into arr of task obj by mapping
-//          let mongoTask = taskList.map((el)=>({task:el}))
-
-//     //checking task list number 
-//         if(taskList.length>50){
-//             await TaskModel.insertMany(mongoTask)
-//             await redisClient.del(Task_Key)
-//             console.log('moved task in mongoDB and delete from redis');
-//         }
-      
-//         await redisClient.set(Task_Key, JSON.stringify(taskList))
-   
-
-//     }catch(err){
-//       console.log(err.message);
-      
-//     }
- 
-   
-// })
-
-
-
-//      client.on('error', (err) => {
-//       console.error('MQTT connection error:', err.message);
-//      });
-
-
-
-
-
-
-
-
-// }
-
-
-
-// module.exports = mqttHandler;
 
 
 
@@ -97,7 +11,8 @@ const Task_Key = 'FULLSTACK_TASK_AMAN';
 
 function mqttHandler() {
 
-  const client = mqtt.connect('mqtt://broker.hivemq.com');
+  const client = mqtt.connect('wss://test.mosquitto.org:8081');
+  
 
   client.on('connect', () => {
     console.log('Connected to MQTT Broker');
